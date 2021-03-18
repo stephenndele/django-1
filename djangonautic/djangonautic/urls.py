95b8d9a06f13b080d01cc0b058e9b9232c9ea994
Stephen Ndele <stephenndele09346@gmail.com>
2021-03-18 08:38:03 +0300

    add homepage and about urls

--- djangonautic/djangonautic/urls.py
+++ djangonautic/djangonautic/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^about/$',),
-    url(r'^$')
+    url(r'^about/$',views.about),
+    url(r'^$',views.homepage),
 ]
