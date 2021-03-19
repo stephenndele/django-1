45cf5a138a921e037d00da7409174501c7db625e
Stephen Ndele <stephenndele09346@gmail.com>
2021-03-19 17:16:50 +0300

    add login views

--- djangonautic/accounts/views.py
+++ djangonautic/accounts/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 
 # Create your views here.
 
 def signup_view(request):
-    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # log userin
+            return redirect('articles:list')
+    else:
+        form = UserCreationForm()
     return render(request, 'accounts/signup.html',{'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            #login user
+            return redirect('articles:list')
+    else:
+        form= AuthenticationForm()
+    return render(request, 'accounts/login.html',{'form': form})
