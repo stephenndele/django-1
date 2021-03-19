705f6692d94dd0880c7e7f577c76fa2475219a94
Stephen Ndele <stephenndele09346@gmail.com>
2021-03-20 00:08:18 +0300

    add styles

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -21,6 +21,9 @@ def article_create(request):
         form = forms.CreateArticle(request.POST,request.FILES)
         if form.is_valid():
             #save to db
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
             return redirect('articles:list')
     else:
      form=forms.CreateArticle()
