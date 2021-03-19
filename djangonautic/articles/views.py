fa75bd6dec55dc28c6b693d3b879bafca788ea6e
Stephen Ndele <stephenndele09346@gmail.com>
2021-03-19 12:52:47 +0300

    article detail

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def article_list(request):
     articles = Article.objects.all().order_by('date')
-    return render(request,'articles/article_list.html',{'articles': articles})
+    return render(request, 'articles/article_list.html',{'articles': articles})
 
-def article_detail(request,slug):
+def article_detail(request, slug):
     return HttpResponse(slug)
