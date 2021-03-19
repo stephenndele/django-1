aeacc061cfb7f85c3eef3a813798c87d1fb2c367
Stephen Ndele <stephenndele09346@gmail.com>
2021-03-19 15:45:46 +0300

    add accouts app

--- djangonautic/djangonautic/urls.py
+++ djangonautic/djangonautic/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.conf.urls import url,include
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -26,3 +28,4 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
